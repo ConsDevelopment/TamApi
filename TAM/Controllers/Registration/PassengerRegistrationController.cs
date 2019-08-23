88005208c6a9d22e2cbd0e1c733b000c48692c4d
BackOfficeSolutions <cons.manzano@backofficesolutions.ph>
2019-08-23 18:46:09 +0800

    092319

---
+++ TAM/Controllers/Registration/PassengerRegistrationController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Tam.Accessor;
+using Tam.Models;
+using Tam.Models.Enums;
+using Tam.NHibernate;
+
+namespace Tam.Controllers.Registration {
+	[EnableCors(origins: "*", headers: "*", methods: "*")] // tune to your needs
+	public class PassengerRegistrationController : ApiController {
+		// GET api/<controller>
+		public IEnumerable<string> Get() {
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/<controller>/5
+		public string Get(int id) {
+			return "value";
+		}
+
+		// POST api/<controller>
+		public async Task<string> Post(PassengerModel value) {
+			string result = "Ok";
+			PasswordHasher ph = new PasswordHasher();
+			var passHash = ph.HashPassword(value.Password);
+			NHibernateDriverStore hds = new NHibernateDriverStore();
+			var driver = new PassengerModel {
+				Email = value.Email,
+				FirstName = value.FirstName,
+				LastName = value.LastName,
+				MiddleName = value.MiddleName,
+				BirthDate = value.BirthDate,
+				Gender = value.Gender,
+				Town = value.Town,
+				Province = value.Province,
+				City = value.City,
+				MobileNumber = value.MobileNumber,
+				Password = passHash
+			};
+			try {
+				await hds.CreateDriverAsync(driver);
+				var nhus = new NHibernateUserStore();
+				var emails = await nhus.GetAllAdminEmailAsync();
+				await Emailer.SendMessage(driver.Email + " For Activation", emails, "Registration");
+			} catch (Exception e) {
+				result = e.Message;
+			}
+			return result;
+		}
+
+		// PUT api/<controller>/5
+		public void Put(int id, [FromBody]string value) {
+		}
+
+		// DELETE api/<controller>/5
+		public void Delete(int id) {
+		}
+	}
+}
---
+++ TAM/Models/Passenger/PassengerModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tam.Models.Enums;
+using FluentNHibernate.Mapping;
+
+namespace Tam.Models {
+	public class PassengerModel {
+		public virtual long Id { get; protected set; }
+		public virtual string Email { get; set; }
+		public virtual string FirstName { get; set; }
+		public virtual string LastName { get; set; }
+		public virtual string MiddleName { get; set; }
+		public virtual DateTime? BirthDate { get; set; }
+		public virtual string Town { get; set; }
+		public virtual string Province { get; set; }
+		public virtual string City { get; set; }
+		public virtual string MobileNumber { get; set; }
+		public virtual string Password { get; set; }
+		public virtual GenderType? Gender { get; set; }
+
+		public virtual String Name() {
+			return ((FirstName ?? "").Trim() + " " + (LastName ?? "").Trim()).Trim();
+		}
+		public PassengerModel() {
+			
+			CreateTime = DateTime.UtcNow;
+		}
+		public virtual DateTime CreateTime { get; set; }
+		public virtual DateTime? DeleteTime { get; set; }
+		public virtual DateTime? UpdateTime { get; set; }
+		
+		public virtual UserModel UpdatedBy { get; set; }
+
+		public class PassengerModelMap : ClassMap<PassengerModel> {
+			public PassengerModelMap() { 
+				Id(x => x.Id);
+				Map(x => x.Email).Index("Email_IDX").Length(30).UniqueKey("uniq");
+				Map(x => x.FirstName).Length(30);
+				Map(x => x.LastName).Length(30);
+				Map(x => x.MiddleName).Length(30);
+				Map(x => x.Gender).Length(10);
+				Map(x => x.DeleteTime);
+				Map(x => x.BirthDate);
+				Map(x => x.CreateTime);
+				Map(x => x.UpdateTime);
+				Map(x => x.Town).Length(20);
+				Map(x => x.Province).Length(20);
+				Map(x => x.City).Length(20);
+				Map(x => x.MobileNumber).Length(20);
+				Map(x => x.Password);
+				References(x => x.UpdatedBy, "UpdatedBy").Cascade.SaveUpdate();
+			}
+		}
+	}
+}
